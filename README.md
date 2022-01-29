0f0bd8922023ca3319d301885a2d06ad9c99bbcd
unknown <proglang704206@gmail.com>
2022-01-29 14:23:25 +0700

    Capture Image Features

---
+++ README.md
@@ -0,0 +1,14 @@
+# First Order Binomial - Table Generator for students
+***P/s: This website is in development***
+-> __Features:__
+- Lock/Unlock table value
+- Create inputs or rows
+- Can get the table's image
+-> __Coming Soon:__
+- Can create more than 1 table
+- Can delete inputs or rows
+- Can auto reset values
+- Fill input with JSON Code
+-> __Bugs:__
+- Can't auto resize the input fields
+- The upper part of the image is hidden after capture the image
--- assets/handle/script.js
+++ assets/handle/script.js
@@ -1,13 +1,3 @@
-function autoResizeInput(e){
-  const previousInputX = e.target.previousElementSibling
-  previousInputX.textContent = e.target.value.trim().length>0?e.target.value:""
-  const inputXValue = previousInputX.textContent
-  e.target.style.width = `${(previousInputX.offsetWidth*1.25)-e.target.value.length}px`;
-  document.querySelectorAll(".solutionOfX>span").forEach(inp=>{
-    inp.style.width = e.target.style.width
-  })
-  if(inputXValue.length>0) e.target.value = inputXValue;
-}
 const inputFieldHTML = `
         <span>
           <span class="table__hidden-field js-hidden-input-x"></span>
@@ -24,11 +14,23 @@ const resolutionXValue = `
     </div>`
 function scaleWidthInput(){
   const inputsX = document.querySelectorAll(".js-input-x")
-  return inputsX.forEach(inputX=>{
+  function autoResizeInput(e){
+    const previousInputX = e.target.previousElementSibling
+    previousInputX.textContent = e.target.value.trim().length>0?e.target.value:""
+    const inputXValue = previousInputX.textContent
+    e.target.style.width = `${(previousInputX.offsetWidth*1.25)-e.target.value.length}px`;
+    document.querySelectorAll(".solutionOfX>span").forEach(inp=>{
+      inp.style.width = e.target.style.width
+    })
+    if(inputXValue.length>0) e.target.value = inputXValue;
+  }
+  inputsX.forEach(inputX=>{
     inputX.oninput=e=>autoResizeInput(e)
     inputX.onchage=e=>autoResizeInput(e)
+    inputX.onkeypress=e=>autoResizeInput(e)
   })
 }
+scaleWidthInput()
 document.querySelectorAll(".addRows").forEach(btn=>{
     btn.onclick=()=>{
       const rows_0 = document.querySelectorAll(".table__row-0--input>span").length
@@ -54,10 +56,10 @@ document.querySelectorAll(".addRows").forEach(btn=>{
       let cell_last = insertRow.insertCell(1)
       cell_first.insertAdjacentHTML("beforeend",inputFieldHTML)
       cell_last.insertAdjacentHTML("beforeend",applyXValue)
+      scaleWidthInput()
       document.querySelectorAll(".solutionOfX").forEach(x=>{
         if(rows_0>0) x.classList.remove("hide")
       })
-      scaleWidthInput()
     }
 })
 document.querySelectorAll(".addRowX").forEach(btn=>{
@@ -81,25 +83,42 @@ document.querySelectorAll('.btn-row').forEach(btnRow=>{
       document.querySelectorAll(".addRowX").forEach(btnAddRowX=>{
         document.querySelectorAll(".addRows").forEach(btnAddRows=>{
           document.querySelectorAll(".table__row-0--input").forEach(elem=>{
-            if(isLocked){
-              btnRow.innerHTML = '<i class="fas fa-unlock"></i> Unlock'
-              elm.classList.add('removeBorder')
-              btnAddRowX.classList.add('hide')
-              btnAddRows.classList.add('btn--disabled')
-              elem.style.marginLeft="45px";
-            } else {
-              btnRow.innerHTML = '<i class="fas fa-lock"></i> Lock'
-              elm.classList.remove('removeBorder')
-              btnAddRowX.classList.remove('hide')
-              btnAddRows.classList.remove('btn--disabled')
-              elem.style.marginLeft="68px"
-            }
+            document.querySelectorAll(".js-convert-img").forEach(cvtBtn=>{
+              if(isLocked){
+                btnRow.innerHTML = '<i class="fas fa-unlock"></i> Unlock'
+                elm.classList.add('removeBorder')
+                btnAddRowX.classList.add('hide')
+                btnAddRows.classList.add('btn--disabled')
+                cvtBtn.classList.remove('btn--disabled')
+                elem.style.marginLeft="45px";
+              } else {
+                btnRow.innerHTML = '<i class="fas fa-lock"></i> Lock'
+                elm.classList.remove('removeBorder')
+                btnAddRowX.classList.remove('hide')
+                btnAddRows.classList.remove('btn--disabled')
+                cvtBtn.classList.add('btn--disabled')
+                elem.style.marginLeft="68px"
+              }
+            })
           })
         })
       })
     })
   }
 })
-// html2canvas(document.querySelector(".")).then(canvas => {
-//     document.body.appendChild(canvas)
-// });
+document.querySelectorAll(".js-convert-img").forEach(convertBtn=>{
+  convertBtn.onclick = () =>{
+    convertBtn.classList.add('btn--disabled')
+    document.querySelectorAll(".container__canvas-title").forEach(title=>{
+      title.innerText = "Image:"
+      title.style.fontSize = "1.6rem"
+      document.querySelectorAll(".js__table").forEach(tb=>{
+        html2canvas(tb).then(canvas => {
+            document.querySelectorAll(".container__canvas-img").forEach(cvs=>{
+              cvs.appendChild(canvas)
+            })
+        });
+      })
+    })
+  }
+})
